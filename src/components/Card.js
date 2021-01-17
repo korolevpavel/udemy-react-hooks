bea0231eea9381813714d351d2790d34535ad85f
Korolev Pavel <korolev.vrn@gmail.com>
2021-01-17 13:38:04 +0300

    Update pages

---
+++ src/components/Card.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+export const Card = () => (
+    <div className="card">
+        <div className="card-body">
+            <img src={''} alt={''} className='card-img-top' />
+            <h5 className="card-title">Title</h5>
+            <Link
+                to={'/profile/'+'react'}
+                className="btn btn-primary">
+                Открыть
+            </Link>
+        </div>
+    </div>
+)
