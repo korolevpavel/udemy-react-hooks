c53ac4f95534eb0bb527d93220fb48e8aac52bcc
Korolev Pavel <korolev.vrn@gmail.com>
2021-01-17 13:20:26 +0300

    Add pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import React from "react";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
+import {Navbar} from "./components/Navbar";
+import {Home} from "./pages/Home";
+import {About} from "./pages/About";
+import {Profile} from "./pages/Profile";
+
 function App() {
-  return (
-    <div className="container pt-4">
-      <h1>React-Hooks</h1>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Navbar />
+            <div className="container pt-4">
+                <Switch>
+                    <Route path="/" exact component={Home}/>
+                    <Route path="/about" component={About}/>
+                    <Route path="/profile/:name" component={Profile}/>
+                </Switch>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
